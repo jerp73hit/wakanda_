da053b1307f7553a8cef7f9b0374bf84e7104fba
JossuaRuiz <jossua0202@hotmail.com>
2025-07-08 19:45:26 -0500

    Update encriptacion imagen

--- js/schedule.js
+++ js/schedule.js
@@ -1,18 +1,19 @@
-
-const activationTime = "2025-07-08T19:40:00";
+const activationTime = "2025-07-07T03:32:00";
 
 document.addEventListener("DOMContentLoaded", () => {
   const now = new Date();
   const activation = new Date(activationTime);
 
-  const qr = document.getElementById("qr");
+  const contenedor = document.getElementById("contenedorQR");
   const espera = document.getElementById("espera");
 
   if (now >= activation) {
-    qr.style.display = "block";
-    espera.style.display = "none";
+    if (espera) espera.style.display = "none";
+    const clave = prompt("Ingresa tu código de acceso:");
+    if (clave) {
+      desencriptarImagen("../qrs/chart.enc", clave);
+    }
   } else {
-    qr.style.display = "none";
-    espera.style.display = "block";
+    if (espera) espera.style.display = "block";
   }
 });
